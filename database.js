f58b1670876120c3a0588db9212237933761e065
PHONGHUB <137273216+Quycai@users.noreply.github.com>
2026-06-20 02:58:36 +0700

    Create database.js

---
+++ database.js
@@ -0,0 +1,32 @@
+// File: database.js
+const dbGame = {
+    "special-harem-class": {
+        title: "[Việt Hóa] Special Harem Class",
+        version: "v0.4.6",
+        author: "Reahe",
+        size: "850",
+        desc: "Câu chuyện xoay quanh một lớp học đặc biệt nơi bạn là nam sinh duy nhất...",
+        cover: "https://i.postimg.cc/m2ZR663V/v-Y0JFq.png",
+        gallery: [
+            "https://i.postimg.cc/m2ZR663V/gameplay1.png",
+            "https://i.postimg.cc/m2ZR663V/gameplay2.png",
+            "https://i.postimg.cc/m2ZR663V/gameplay3.png",
+            "https://i.postimg.cc/m2ZR663V/gameplay4.png"
+        ],
+        driveLink: "https://drive.google.com/..."
+    },
+    "a-genin-tale": {
+        title: "[Việt Hóa] A Genin Tale",
+        version: "v0.3",
+        author: "Luchetty",
+        size: "245",
+        desc: "Vào vai một Genin trẻ tuổi tại Làng Lá, bắt đầu hành trình nhẫn giả của riêng bạn...",
+        cover: "https://i.postimg.cc/fbbbS1dC/1777495289444.avif",
+        gallery: [
+            "https://i.postimg.cc/fbbbS1dC/ảnh1.avif",
+            "https://i.postimg.cc/fbbbS1dC/ảnh2.avif"
+        ],
+        driveLink: "https://mega.nz/..."
+    }
+    // Ông có 1000 game thì cứ điền tiếp xuống dưới, y như nhập Excel!
+};
